f9d3b8835ab60451c110fbd526fd56fc5a5d3e21
hycho04 <hycho8901@gmail.com>
2026-04-29 05:22:43 +0900

    수정

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import express from "express";
-
-const app = express();
-
-app.listen(3000, () => {
-  console.log("hello world");
-});
+import express from 'express';
 
 import storeRouter from './routes/store.route';
 import reviewRouter from './routes/review.route';
 import missionRouter from './routes/mission.route';
 
+const app = express();
+
+app.use(express.json());
+
 app.use(storeRouter);
 app.use(reviewRouter);
 app.use(missionRouter);
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
