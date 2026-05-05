1d01719713a99811e103f6d0983e126f3d353d64
eunseocandoit <eunseocandoit@gmail.com>
2026-05-05 23:53:45 +0900

    mission: 6주차 미션 수행

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
-import { handleListStoreReviews } from "./modules/stores/controllers/store.controller.js";
+import {
+  handleListStoreReviews,
+  handleListMyReviews,
+  handleListStoreMissions,
+  handleListMyMissions,
+  handleCompleteMission,
+} from "./modules/stores/controllers/store.controller.js";
 
 const app = express();
 app.use(express.json());
 
 app.get("/api/v1/stores/:storeId/reviews", handleListStoreReviews);
+app.get("/api/v1/users/:userId/reviews", handleListMyReviews);
+app.get("/api/v1/stores/:storeId/missions", handleListStoreMissions);
+app.get("/api/v1/users/:userId/missions", handleListMyMissions);
+app.patch("/api/v1/user-missions/:userMissionId/complete", handleCompleteMission);
 
 app.listen(3000, () => {
   console.log("🔥 서버 실행됨: http://localhost:3000");
--- src/modules/stores/repositories/store.repository.ts
+++ src/modules/stores/repositories/store.repository.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../../../db.config.js";
 
 export const getAllStoreReviews = async (storeId: number, cursor: number) => {
-  const reviews = await prisma.userStoreReview.findMany({
+  return await prisma.userStoreReview.findMany({
     select: {
       id: true,
       content: true,
@@ -15,6 +15,47 @@ export const getAllStoreReviews = async (storeId: number, cursor: number) => {
     orderBy: { id: "asc" },
     take: 5,
   });
+};
 
-  return reviews;
+export const getMyReviews = async (userId: number, cursor: number) => {
+  return await prisma.review.findMany({
+    where: {
+      userId,
+      id: { gt: cursor },
+    },
+    include: { restaurant: true },
+    orderBy: { id: "asc" },
+    take: 5,
+  });
+};
+
+export const getStoreMissions = async (storeId: number, cursor: number) => {
+  return await prisma.mission.findMany({
+    where: {
+      restaurantId: storeId,
+      id: { gt: cursor },
+    },
+    orderBy: { id: "asc" },
+    take: 5,
+  });
+};
+
+export const getMyMissions = async (userId: number, cursor: number) => {
+  return await prisma.userMission.findMany({
+    where: {
+      userId,
+      status: "진행중",
+      id: { gt: cursor },
+    },
+    include: { mission: true },
+    orderBy: { id: "asc" },
+    take: 5,
+  });
+};
+
+export const completeMission = async (userMissionId: number) => {
+  return await prisma.userMission.update({
+    where: { id: userMissionId },
+    data: { status: "진행완료" },
+  });
 };
