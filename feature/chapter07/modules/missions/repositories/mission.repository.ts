b9e3213c711c5f86169d45c920fb9000c0aaeb04
uone12 <164721035+uone12@users.noreply.github.com>
2026-05-18 18:01:57 +0900

    Update mission.repository.ts

--- feature/chapter07/modules/missions/repositories/mission.repository.ts
+++ feature/chapter07/modules/missions/repositories/mission.repository.ts
@@ -1,26 +1,18 @@
-import { StatusCodes } from "http-status-codes";
 import { prisma } from "../../../db.config.js";
-import { CustomError } from "../../../common/errors/customError.js";
 
-export const completeUserMission = async (
+export const findUserMission = async (
   userId: number,
   userMissionId: number
 ) => {
-  const userMission = await prisma.userMission.findFirst({
+  return await prisma.userMission.findFirst({
     where: {
       id: userMissionId,
       userId,
     },
   });
+};
 
-  if (!userMission) {
-    throw new CustomError(StatusCodes.NOT_FOUND, "진행 중인 미션을 찾을 수 없습니다.");
-  }
-
-  if (userMission.status === "COMPLETE") {
-    throw new CustomError(StatusCodes.BAD_REQUEST, "이미 완료된 미션입니다.");
-  }
-
+export const updateUserMissionToComplete = async (userMissionId: number) => {
   return await prisma.userMission.update({
     where: { id: userMissionId },
     data: { status: "COMPLETE" },
